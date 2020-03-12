46893f23769436de2d72bf7fea0e4631dcb730b5
pikatech <pikatech@mail.de>
2020-03-12 10:58:33 +0100

    Make it possible to resolve conjunctions
    hearst will now return relations for all parts of a conjunction and not just the first one.

--- scicopia_tools/analyzers/hearst.py
+++ scicopia_tools/analyzers/hearst.py
@@ -6,14 +6,47 @@ Created on Wed Jan 15 13:55:23 2020
 @author: kampe
 """
 
+from functools import cmp_to_key
 import itertools
 import logging
 from typing import List, Tuple
 
 import spacy
+from spacy.tokens.span import Span
 import networkx as nx
 
 NLP = spacy.load("en_core_web_lg", disable=["ner"])
+
+
+def interval_sort(entity1, entity2) -> int:
+    if entity1[0] < entity2[0]:
+        return -1
+    if entity1[0] > entity2[0]:
+        return 1
+    return entity2[1] - entity1[1]
+
+
+IntervalKey = cmp_to_key(interval_sort)
+
+
+def conflate_conjuncts(chunks: List[Span]) -> List[List[Span]]:
+    intervals = []
+    for c in chunks:
+        parts = c.conjuncts
+        intervals.append((min(min(map(lambda x: x.i, parts)), c.start), max(max(map(lambda x: x.i, parts)), c.end-1), c) if parts else (c.start, c.end - 1, c))
+
+    intervals.sort(key=IntervalKey)
+    filtered = []
+    start = -1
+    end = -1
+    for interval in intervals:
+        if interval[0] >= start and interval[1] <= end:
+            filtered[len(filtered)-1].append(interval[2])
+            continue
+        filtered.append([interval[2]])
+        start = interval[0]
+        end = interval[1]
+    return filtered
 
 
 def hearst(text: str) -> List[Tuple[str]]:
@@ -44,18 +77,23 @@ def hearst(text: str) -> List[Tuple[str]]:
 
     graph = nx.Graph(edges)
     candidates = list(doc.noun_chunks)
+    candidates = conflate_conjuncts(candidates)
 
     hits = []
     for source, target in itertools.combinations(candidates, 2):
-        source_root = source.root.i
-        target_root = target.root.i
-        logging.debug("%d->%d:", source.root.i, target.root.i)
-        path = nx.shortest_path(graph, source=source_root, target=target_root)
-        logging.debug([doc[x].text for x in path])
-        if (
-            len(path) == 3
-            and doc[path[1]].text == "as"
-            and doc[path[1] - 1].text == "such"
-        ):
-            hits.append((source.lemma_, "such as", target.lemma_))
+        source_root = source[0].root.i
+        target_root = target[0].root.i
+        logging.debug("%d->%d:", source[0].root.i, target[0].root.i)
+        try:
+            path = nx.shortest_path(graph, source=source_root, target=target_root)
+            logging.debug([doc[x].text for x in path])
+            if (
+                len(path) == 3
+                and doc[path[1]].text == "as"
+                and doc[path[1] - 1].text == "such"
+            ):
+                for t in target:
+                    hits.append((source[0].lemma_, "such as", t.lemma_))
+        except nx.NetworkXNoPath:
+            pass
     return hits
