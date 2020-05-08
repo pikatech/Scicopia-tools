bc1c27e820662a071e2f847991f40a408dfb9cfd
Tech <pikatech@mail.de>
2020-05-08 15:59:38 +0200

    Fix test

--- scicopia_tools/tests/test_hearst.py
+++ scicopia_tools/tests/test_hearst.py
@@ -6,11 +6,12 @@ Created on Fri Mar  6 15:23:40 2020
 @author: kampe
 """
 
-from analyzers.hearst import hearst
+from analyzers.Hearst import Hearst
 
 
 def test_such_as():
+    hearst = Hearst()
     text = "Patients of all ages can develop nail disorders, such as onychocryptosis, which are recurrent and painful conditions."
-    relations = hearst(text)
+    relations = hearst.process(text)
     assert len(relations) == 1
     assert " ".join(relations[0]) == "nail disorder such as onychocryptosis"
