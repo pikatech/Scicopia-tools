fd4d1239044383b3f8688b2c3962cb985e3e320c
Tech <pikatech@mail.de>
2020-05-26 18:02:30 +0200

    Fix test

--- scicopia_tools/tests/test_hearst.py
+++ scicopia_tools/tests/test_hearst.py
@@ -10,8 +10,8 @@ from analyzers.Hearst import Hearst
 
 
 def test_such_as():
-    hearst = Hearst()
+    hearst = Hearst("en_core_web_sm")
     text = "Patients of all ages can develop nail disorders, such as onychocryptosis, which are recurrent and painful conditions."
-    relations = hearst.process(text)
+    relations = hearst.process(text)[Hearst.field]
     assert len(relations) == 1
     assert " ".join(relations[0]) == "nail disorder such as onychocryptosis"
