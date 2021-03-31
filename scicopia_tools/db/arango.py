2605d081105e27044d34a05032afddb0fcf7311f
RonjaKarmann <ronja.maria.karmann@uni-jena.de>
2021-03-31 18:35:03 +0200

    more raised errors and wording changes

--- scicopia_tools/db/arango.py
+++ scicopia_tools/db/arango.py
@@ -32,34 +32,41 @@ def setup() -> DbAccess:
 
     Raises
     ------
+    ConfigError
+        If a needed entry is missing in the config file.
     DBError
-        If the database or the collection can not be found.
+        If the connection to the ArangoDB server failed or the database or the collection can not be found.
     """
     config = read_config()
-    if "arango_url" in config:
-        connection = Connection(
-            arangoURL=config["arango_url"],
-            username=config["username"],
-            password=config["password"],
-        )
-    else:
-        connection = Connection(
-            username=config["username"], password=config["password"]
-        )
+    if not "username" in config:
+        raise ConfigError("Setting missing in config file: 'username'.")
+    if not "password" in config:
+        raise ConfigError("Setting missing in config file: 'password'.")
+    try:
+        if "arango_url" in config:
+            connection = Connection(
+                arangoURL=config["arango_url"],
+                username=config["username"],
+                password=config["password"],
+            )
+        else:
+            connection = Connection(
+                username=config["username"], password=config["password"]
+            )
+    except:
+        raise DBError(f"Connection to the ArangoDB server failed.")
 
     if not "database" in config:
-        raise ConfigError("Setting missing in config file: 'database'")
+        raise ConfigError("Setting missing in config file: 'database'.")
     if connection.hasDatabase(config["database"]):
         db = connection[config["database"]]
     else:
-        logging.error("Database %s not found.", config["database"])
-        raise DBError(f"Database {config['database']} not found.")
+        raise DBError(f"Database '{config['database']}' not found.")
 
     if not "documentcollection" in config:
-        raise ConfigError("Setting missing in config file: 'documentcollection'")
+        raise ConfigError("Setting missing in config file: 'documentcollection'.")
     if db.hasCollection(config["documentcollection"]):
         collection = db[config["documentcollection"]]
     else:
-        logging.error("Collection %s not found.", config["documentcollection"])
-        raise DBError(f"Collection {config['documentcollection']} not found.")
+        raise DBError(f"Collection '{config['documentcollection']}' not found.")
     return DbAccess(collection, connection, db)
